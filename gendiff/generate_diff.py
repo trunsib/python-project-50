62128a7ae60b66a039c801caa80b343cd74f77a9
trun <e.s.trunov@gmail.com>
2025-10-27 16:32:27 +0700

    gendiff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,8 +3,8 @@ from gendiff.diff import diff
 from gendiff.formats.format import format_diff
 
 
-def generate_diff(path_file1, path_file2, format_name='stylish'):
-    d1 = get_dict_from_file(path_file1)
-    d2 = get_dict_from_file(path_file2)
+def generate_diff(filepath1, filepath2, format_name='stylish'):
+    d1 = get_dict_from_file(filepath1)
+    d2 = get_dict_from_file(filepath2)
     l_diff = diff(d1, d2)
     return format_diff(l_diff, format_name)
