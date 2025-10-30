a893f875723740d3c06b5257019b350f2e387cbc
trun <e.s.trunov@gmail.com>
2025-10-30 20:34:54 +0700

    gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-
 from gendiff.parser import parser_arg
 from gendiff.generate_diff import generate_diff
 
 
 def main():
-
     filepath1, filepath2, format_name = parser_arg()
     diff = generate_diff(filepath1, filepath2, format_name)
     print(diff)
 
 
 if __name__ == "__main__":
-    
     main()
