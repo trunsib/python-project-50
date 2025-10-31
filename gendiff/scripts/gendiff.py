411dbcb92963c44d16b19cd19de5d4675003c8e6
trun <e.s.trunov@gmail.com>
2025-10-31 18:26:31 +0700

    gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,10 +4,14 @@ from gendiff.generate_diff import generate_diff
 
 
 def main():
+   
+   
     filepath1, filepath2, format_name = parser_arg()
     diff = generate_diff(filepath1, filepath2, format_name)
     print(diff)
 
 
 if __name__ == "__main__":
+
+    
     main()
