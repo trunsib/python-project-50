9803800af212a89b1ca00955667509bc86c50388
trun <e.s.trunov@gmail.com>
2026-05-09 00:49:44 +0700

    gendiff

--- gendiff/formattеrs/stylish.py
+++ gendiff/formattеrs/stylish.py
@@ -1,46 +1,60 @@
-def create_stylish(d_list, lvl=0):
-    res = []
-    res.append('{\n')
-    ind = ' ' * 2
-    ind = ind + ind * 2 * lvl
-    d_list.sort(key=lambda x: x['name'])
-    for node in d_list:
-        op = ' '
-        match node['status']:
-            case 'nested':
-                data = create_stylish(node['children'], lvl + 1)
-            case 'added':
-                data = сonvert_to_string(node['data'], ind)
-                op = '+'
-            case 'deleted':
-                data = сonvert_to_string(node['data'], ind)
-                op = '-'
-            case 'changed':
-                data = сonvert_to_string(node['data before'], ind)
-                res.append(f"{ind}- {node['name']}: {data}\n")
-                data = сonvert_to_string(node['data after'], ind)
-                op = '+'
-            case 'not changed':
-                data = сonvert_to_string(node['data'], ind)
-            case _:
-                raise ValueError('Invalid type!')
-        res.append(f"{ind}{op} {node['name']}: {data}\n")
-    res.append(ind[:-2] + '}')
-    return ''.join(res)
+def create_stylish(d_list, depth=0):
+    """Format diff list to stylish string format."""
+    indent = '    ' * depth
+    current_indent = '    ' * (depth + 1)
+    
+    lines = ['{']
+    
+    for node in sorted(d_list, key=lambda x: x['name']):
+        name = node['name']
+        status = node['status']
+        
+        if status == 'nested':
+            value = create_stylish(node['children'], depth + 1)
+            lines.append(f"{current_indent}{name}: {value}")
+        
+        elif status == 'added':
+            value = convert_to_string(node['data'])
+            lines.append(f"{current_indent}+ {name}: {value}")
+        
+        elif status == 'deleted':
+            value = convert_to_string(node['data'])
+            lines.append(f"{current_indent}- {name}: {value}")
+        
+        elif status == 'changed':
+            old_value = convert_to_string(node['data before'])
+            new_value = convert_to_string(node['data after'])
+            lines.append(f"{current_indent}- {name}: {old_value}")
+            lines.append(f"{current_indent}+ {name}: {new_value}")
+        
+        elif status == 'not changed':
+            value = convert_to_string(node['data'])
+            lines.append(f"{current_indent}  {name}: {value}")
+    
+    lines.append(f"{indent}}}")
+    
+    result = '\n'.join(lines)
+    return result + '\n'
 
 
-def сonvert_to_string(data, ind):
-    if type(data) is dict:
-        ind = ind + '    '
-        res = '{\n'
-        for key in data.keys():
-            value = сonvert_to_string(data[key], ind)
-            res = res + ind + '  ' + key + ': ' + value + '\n'
-        res = res + ind[:-2] + '}'
-    elif data is False or data is True:
-        res = str(data).lower()
-    elif data is None:
-        res = 'null'
-    else:
-        res = str(data)
-    return res
+def convert_to_string(value):
+    """Convert a value to string representation."""
+    if isinstance(value, dict):
+        if not value:
+            return '{}'
+        lines = ['{']
+        for k, v in sorted(value.items()):
+            lines.append(f"    {k}: {convert_to_string(v)}")
+        lines.append('}')
+        return '\n'.join(lines)
+    
+    if isinstance(value, bool):
+        return str(value).lower()
+    
+    if value is None:
+        return 'null'
+    
+    if isinstance(value, str):
+        return value
+    
+    return str(value)
