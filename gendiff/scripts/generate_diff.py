1bb1473109d317af346a8e8f8b33d45cf3518700
trun <e.s.trunov@gmail.com>
2026-05-12 19:10:04 +0700

    gendiff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -7,4 +7,4 @@ def generate_diff(path_file1, path_file2, format_name='stylish'):
     d1 = get_dict_from_file(path_file1)
     d2 = get_dict_from_file(path_file2)
     l_diff = diff(d1, d2)
-    return format_diff(l_diff, format_name)
+    return format_diff(l_diff, format_name) + '\n'
