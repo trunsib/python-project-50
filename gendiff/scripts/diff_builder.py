0e6ca4ca42366f2cdef29660632e2c1754d5dbf4
trun <e.s.trunov@gmail.com>
2026-05-13 11:05:21 +0700

    gendiff

--- gendiff/scripts/diff_builder.py
+++ gendiff/scripts/diff_builder.py
@@ -4,20 +4,20 @@ def diff(d1, d2):
     for key in keys:
         node = {'name': key}
         if key not in d1:
-            node['status'] = 'added'
-            node['data'] = d2[key]
+            node['type'] = 'added'
+            node['value'] = d2[key]
         elif key not in d2:
-            node['status'] = 'deleted'
-            node['data'] = d1[key]
+            node['type'] = 'deleted'
+            node['value'] = d1[key]
         elif type(d1[key]) is dict and type(d2[key]) is dict:
-            node['status'] = 'nested'
+            node['type'] = 'nested'
             node['children'] = diff(d1[key], d2[key])
         elif d1[key] == d2[key]:
-            node['status'] = 'not changed'
-            node['data'] = d1[key]
+            node['type'] = 'unchanged'
+            node['value'] = d1[key]
         else:
-            node['status'] = 'changed'
-            node['data before'] = d1[key]
-            node['data after'] = d2[key]
+            node['type'] = 'changed'
+            node['old_value'] = d1[key]
+            node['new_value'] = d2[key]
         res.append(node)
     return res
