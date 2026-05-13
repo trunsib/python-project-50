86d02f79a67c46227ceb1c056760f32ff2f3d7bd
trun <e.s.trunov@gmail.com>
2026-05-13 10:47:55 +0700

    gendiff

--- gendiff/formattеrs/stylish.py
+++ gendiff/formattеrs/stylish.py
@@ -8,39 +8,39 @@ def create_stylish(d_list, lvl=0):
         op = ' '
         match node['type']:
             case 'nested':
-                value = create_stylish(node['children'], lvl + 1)
+                data = create_stylish(node['children'], lvl + 1)
             case 'added':
-                value = сonvert_to_string(node['value'], ind)
+                data = сonvert_to_string(node['value'], ind)
                 op = '+'
             case 'deleted':
-                value = сonvert_to_string(node['value'], ind)
+                data = сonvert_to_string(node['value'], ind)
                 op = '-'
             case 'changed':
-                value = сonvert_to_string(node['old_value'], ind)
-                res.append(f"{ind}- {node['name']}: {value}\n")
-                value = сonvert_to_string(node['new_value'], ind)
+                data = сonvert_to_string(node['old_value'], ind)
+                res.append(f"{ind}- {node['name']}: {data}\n")
+                data = сonvert_to_string(node['new_value'], ind)
                 op = '+'
             case 'unchanged':
-                value = сonvert_to_string(node['value'], ind)
+                data = сonvert_to_string(node['value'], ind)
             case _:
                 raise ValueError('Invalid type!')
-        res.append(f"{ind}{op} {node['name']}: {value}\n")
+        res.append(f"{ind}{op} {node['name']}: {data}\n")
     res.append(ind[:-2] + '}')
     return ''.join(res)
 
 
-def сonvert_to_string(value, ind):
-    if type(value) is dict:
+def сonvert_to_string(data, ind):
+    if type(data) is dict:
         ind = ind + '    '
         res = '{\n'
-        for key in value.keys():
-            value = сonvert_to_string(value[key], ind)
+        for key in data.keys():
+            value = сonvert_to_string(data[key], ind)
             res = res + ind + '  ' + key + ': ' + value + '\n'
         res = res + ind[:-2] + '}'
-    elif value is False or value is True:
+    elif data is False or data is True:
         res = str(value).lower()
-    elif value is None:
+    elif data is None:
         res = 'null'
     else:
-        res = str(value)
+        res = str(data)
     return res
